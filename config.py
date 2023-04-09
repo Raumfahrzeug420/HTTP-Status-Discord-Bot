a1d03a81eb332db07d62df9bce76de7179232d6a
ໂນອາ <109697122+Raumfahrzeug420@users.noreply.github.com>
2023-04-09 01:20:35 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+token = ""
+seconds = ""
+server = "" #The website/server you want to ping
+channelid = "" #VC Channel ID
+offlinename = "" #What to name the channel when offline
+onlinename = "" #What to name the channel when onlne
