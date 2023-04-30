e620e96357ba2a5a3e683892fbae6947bb8ee396
ໂນອາ <109697122+Raumfahrzeug420@users.noreply.github.com>
2023-04-30 13:39:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -53,11 +53,11 @@ async def ServerCheckLoop():
         status = "Offline!"
     await client.change_presence(activity=discord.Game(name=f"{server} Status: {status}"))
 
-@client.command(pass_context=True)
+@client.hybrid_command(pass_context=True)
 async def ping(ctx):
 	await ctx.send("> `Pong! " + str(round(client.latency * 1000)) + "ms`")
 
-@client.command(pass_context=True)
+@client.hybrid_command(pass_context=True)
 async def status(ctx):
     param = "-n" if platform.system().lower()=="windows" else "-c"
     check = os.system(f"ping {param} 1 {server}")
@@ -65,21 +65,7 @@ async def status(ctx):
         status = "Online"
     else:
         status = "Offline!" 
-    await ctx.send(f"Server Status: {status}")
-
-@client.tree.command(name="ping")
-async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message("> `Pong! " + str(round(client.latency * 1000)) + "ms`")
-
-@client.tree.command(name="status")
-async def ping(interaction: discord.Interaction):
-    param = "-n" if platform.system().lower()=="windows" else "-c"
-    check = os.system(f"ping {param} 1 {server}")
-    if check == 0:
-        status = "Online"
-    else:
-        status = "Offline!" 
-    await interaction.response.send_message(f"Server Status: {status}")
+    await ctx.send(f"Server Status: {status}")]
 
 @ServerCheckLoop.before_loop
 async def before_some_task():
