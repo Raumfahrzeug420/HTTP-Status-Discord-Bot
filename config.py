3168fc849a4c012cce9f4c1b247111b175ddca3e
ໂນອາ <109697122+Raumfahrzeug420@users.noreply.github.com>
2023-04-30 13:46:53 -0400

    Delete config.py
    Main.py will automatically creeate the config.py file.

--- config.py
+++
@@ -1,7 +0,0 @@
-server = "" #The website/server you want to ping
-channelid = "" #VC Channel ID
-offlinename = "" #What to name the channel when offline
-onlinename = "" #What to name the channel when onlne
-prefix = "" #Prefix for commands
-token = "" #Bot Token
-seconds = "" #How many seconds till it pings the website/server.
